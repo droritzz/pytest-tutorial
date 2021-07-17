70d43942898d3289bad1b01a157ece227871bdec
droritzz <80842330+droritzz@users.noreply.github.com>
2021-07-17 19:01:49 +0300

    Create setup.py
    creating setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="MyWallet",
+    version='0.1.0',
+    description="A pytest tuturial",
+    author="droritzz for Otomator Otomator",
+    author_email="contact@otomato.io",
+    url="https://github.com/droritzz/pytest-tutorial.git",
+    license="GPLv3+",
+    
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.8'
+    )
+)
