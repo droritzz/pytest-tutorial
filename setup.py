47def0637fe23972ca816480a46e4f0db59d4b06
droritzz <80842330+droritzz@users.noreply.github.com>
2021-07-17 19:06:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="MyWallet",
-    version='0.1.0',
+    version='0.0.1',
     description="A pytest tuturial",
     author="droritzz for Otomator Otomator",
     author_email="contact@otomato.io",
